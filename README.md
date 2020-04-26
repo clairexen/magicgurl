bfb0e9f5bb46f34cedd1978053a9c1acd0168a33
Claire Wolf <claire@symbioticeda.com>
2020-04-26 15:34:41 +0200

    Initial import
    Signed-off-by: Claire Wolf <claire@symbioticeda.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+A Chrome extension that replaces the t-slur (and similar words) with 'magicgurl' on porn sites
+----------------------------------------------------------------------------------------------
+
+Install instructions:
+- Put these files in a directory somewhere
+- Open chrome://extensions/ in Chrome
+- Click "Load unpacked" and select the directory
+
+Add more sites:
+- Simply add them to the "matches" section in manifest.json.
+
+Add more words:
+- Simply add them to the JavaScript code in magicgurl.js.
---
+++ magicgurl.js
@@ -0,0 +1,29 @@
+function magicgurl() {
+	// based on https://github.com/nkhil/Brexit_means_Breadsticks/blob/master/content.js
+	console.log("Running Magicgurl text search and replace.");
+	var el = [...document.body.getElementsByTagName('*')];
+	el.forEach(element => {
+		element.childNodes.forEach(child => {
+			if (child.nodeType === 3) {
+				let text = child.nodeValue;
+
+				text = text.replace(/Tranny/g, 'Magicgurl');
+				text = text.replace(/tranny/g, 'magicgurl');
+				text = text.replace(/Trannie/g, 'Magicgurl');
+				text = text.replace(/trannie/g, 'magicgurl');
+
+				text = text.replace(/Sissy/g, 'Magicgurl');
+				text = text.replace(/Sissy/g, 'magicgurl');
+				text = text.replace(/Sissie/g, 'Magicgurl');
+				text = text.replace(/Sissie/g, 'magicgurl');
+
+				text = text.replace(/Shemale/g, 'Magicgurl');
+				text = text.replace(/shemale/g, 'magicgurl');
+
+				child.nodeValue = text;
+			}
+		});
+	});
+}
+
+window.onload = magicgurl();
