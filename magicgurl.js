481fdc63ff15d36b3153cfa85f891432df9f2756
Claire Wolf <claire@symbioticeda.com>
2020-04-27 13:38:29 +0200

    Add more words
    Signed-off-by: Claire Wolf <claire@symbioticeda.com>

--- magicgurl.js
+++ magicgurl.js
@@ -7,6 +7,8 @@ function magicgurl() {
 			if (child.nodeType === 3) {
 				let text = child.nodeValue;
 
+				// trans women => magicgurl
+
 				text = text.replace(/Tranny/g, 'Magicgurl');
 				text = text.replace(/tranny/g, 'magicgurl');
 				text = text.replace(/Trannie/g, 'Magicgurl');
@@ -20,8 +22,25 @@ function magicgurl() {
 				text = text.replace(/Shemale/g, 'Magicgurl');
 				text = text.replace(/shemale/g, 'magicgurl');
 
+				text = text.replace(/Shim/g, 'Magicgurl');
+				text = text.replace(/shim/g, 'magicgurl');
+
 				text = text.replace(/Ladyboy/g, 'Magicgurl');
 				text = text.replace(/ladyboy/g, 'magicgurl');
+
+				text = text.replace(/Dickgirl/g, 'Magicgurl');
+				text = text.replace(/dickgirl/g, 'magicgurl');
+
+				text = text.replace(/Trap/g, 'Magicgurl');
+				text = text.replace(/trap/g, 'magicgurl');
+
+				// trans men => magicboi
+
+				text = text.replace(/Cuntboy/g, 'Magicboi');
+				text = text.replace(/cuntboy/g, 'magicboi');
+
+				text = text.replace(/Hefemale/g, 'Magicboi');
+				text = text.replace(/hefemale/g, 'magicboi');
 
 				child.nodeValue = text;
 			}
